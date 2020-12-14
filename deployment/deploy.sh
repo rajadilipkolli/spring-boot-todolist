116634cf0c35e1808f2b9ad25ca681ec311611c1
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2020-12-14 11:59:30 +0530

    Infra setup using Vagrant and deploy app running as jar

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,17 +5,15 @@ declare USER_AT_SERVER="$USER@$SERVER"
 declare JAR_NAME="spring-boot-todolist-0.0.1-SNAPSHOT.jar"
 declare JAR_PATH="../build/libs/$JAR_NAME"
 declare APP_DIR_ON_SERVER="/home/vagrant/todolist/"
-declare SVC_NAME="todolist.service"
 
 ssh "$USER_AT_SERVER" mkdir -p $APP_DIR_ON_SERVER
 scp $JAR_PATH "$USER_AT_SERVER":$APP_DIR_ON_SERVER
-scp todolist.service "$USER_AT_SERVER":$APP_DIR_ON_SERVER
-scp overrides.properties "$USER_AT_SERVER":$APP_DIR_ON_SERVER
+scp todolist.sh "$USER_AT_SERVER":$APP_DIR_ON_SERVER
+scp overrides.properties "$USER_AT_SERVER":$APP_DIR_ON_SERVER/application.properties
 
 ssh "$USER_AT_SERVER" /bin/bash << EOF
-    sudo mv "$APP_DIR_ON_SERVER"/$SVC_NAME /etc/systemd/system/
-    chmod a+x "$APP_DIR_ON_SERVER"/$JAR_NAME
-    sudo systemctl restart $SVC_NAME
-    sudo systemctl enable $SVC_NAME
-    sudo systemctl daemon-reload
+    cd $APP_DIR_ON_SERVER
+    chmod a+x $JAR_NAME
+    chmod a+x todolist.sh
+    nohup ./todolist.sh restart > nohup.out 2> nohup.err < /dev/null &
 EOF
